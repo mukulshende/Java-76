b4154c1103c72f86e158abcd478cabb52f81f0f1
mukulshende <mukulshende20@gmail.com>
2024-06-22 16:11:56 +0530

    by mukul

--- GitCrud/src/main/java/com/cjc/main/model/Student.java
+++ GitCrud/src/main/java/com/cjc/main/model/Student.java
@@ -1,5 +1,29 @@
 package com.cjc.main.model;
 
-public class Student {
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@Entity
+public class Student 
+{
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.AUTO)
+	 private int id;
+	 private int rollno;
+	 private String name;
+	 private String address;
+	 private String email;
+	 private long mobileno;
+	
+	 
 
 }
