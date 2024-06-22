9312f62672425c1b7630699e05aeff2b8afab40c
mukulshende <mukulshende20@gmail.com>
2024-06-22 11:42:36 +0530

    by mukul

---
+++ GitDemo/src/com/cjc/Git.java
@@ -0,0 +1,16 @@
+package com.cjc;
+
+public class Git {
+	
+	public void m1()
+	{
+		System.out.println("This is m1 Method");
+	}
+	
+	public static void main(String args[])
+	{
+		Git git=new Git();
+		git.m1();
+	}
+
+}
