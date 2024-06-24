448a7a923a4198a9ec88137797d4386c61ae3a3d
mukulshende <mukulshende20@gmail.com>
2024-06-24 15:12:13 +0530

    By mukul

--- GitCrud/src/main/java/com/cjc/main/controller/StudentController.java
+++ GitCrud/src/main/java/com/cjc/main/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.cjc.main.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +26,13 @@ public class StudentController
 		Student stu= studentService.postStudent(st);
 		return new ResponseEntity<Student>(stu,HttpStatus.OK);
 		
+	}
+	
+	@GetMapping("/getAll")
+	public ResponseEntity<List<Student>> getAllStudent()
+	{
+		List<Student> st=studentService.getAllStudentData();
+		return new ResponseEntity<List<Student>>(st,HttpStatus.OK);
 	}
 
 }
--- GitCrud/src/main/java/com/cjc/main/service/StudentServiceImpl.java
+++ GitCrud/src/main/java/com/cjc/main/service/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cjc.main.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,12 @@ public class StudentServiceImpl  implements StudentService{
 	public Student postStudent(Student st) {
 		Student stu=sr.save(st);
 		return stu;
+	}
+
+	@Override
+	public List<Student> getAllStudentData() {
+		Iterable<Student> st=sr.findAll();
+		return (List<Student>)st;
 	}
 
 }
